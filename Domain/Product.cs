2f7c55bd37a1f23207f1436675f9ee724598fc71
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2024-05-15 16:18:41 -0300

    feat: create Product.cs

---
+++ Domain/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace LancheControl.Domain
+{
+    public class Product
+    {
+        [Key]
+        public long Id { get; set; }
+        
+        [Required(ErrorMessage="O nome do produto é obrigatório",AllowEmptyStrings=false)]
+        [MaxLength(100, ErrorMessage = "Nome do produto não pode ultrapassar 100 caracteres.")]
+        public string? Name { get; set; }
+
+        public string? Description { get; set; } = string.Empty;
+
+        [Precision(10,2)]
+        public Decimal Price { get; set; }
+    }
+}
