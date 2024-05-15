4efa1cca832de9dad7f0391584bf3d6039c5d64e
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2024-05-15 10:16:38 -0300

    feat: modify entity Employee.cs

--- Domain/Employee.cs
+++ Domain/Employee.cs
@@ -1,13 +1,22 @@
-﻿using LancheControl.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using LancheControl.Enums;
 
 namespace LancheControl.Domain
 {
     public class Employee
     {
+        [Key]
         public long Id { get; set; }
+        
+        [Required(ErrorMessage="O nome do funcionário é obrigatório",AllowEmptyStrings=false)]
+        public string? Name { get; set; }
 
-        public string Name { get; set; } = string.Empty;
-
+        [Required(ErrorMessage="O cargo do funcionário é obrigatório")] 
         public Role Role { get; set; }
+        
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        
     }
 }
